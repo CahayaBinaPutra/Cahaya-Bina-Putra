abe8779e900399cff48f2f0143c7d37e9bf855ce
CahayaBinaPutra <bprcbt7@gmail.com>
2024-09-06 21:44:41 -0700

    Update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -14,6 +14,7 @@ import ChatBot from './components/ChatBot';
 
 function App() {
   return (
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
@@ -27,6 +28,7 @@ function App() {
       <Route path="/search-results" element={<SearchResults />} />
       <Route path="/Chatbot" element={<ChatBot />} />
     </Routes>
+      </HashRouter>
   );
 }
 
